a810496e1e85a4e22841c37865134775170cbad2
Pavel Vlasov <freakycue@gmail.com>
2016-04-07 14:35:56 +0300

    small fix for test component

--- src/component.jsx
+++ src/component.jsx
@@ -2,10 +2,13 @@
 
 import React from 'react';
 
-export default function(props) {
-  return (
-    <div>
-      <h1>Hello {props.name}!</h1>
-    </div>
-  );
+export default
+class Component extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello {this.props.name}!</h1>
+      </div>
+    );
+  }
 }
